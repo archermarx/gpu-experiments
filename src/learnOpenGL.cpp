caf2ef11524cecdf3cc0ca2ec69af627ee20544b
Thomas Marks <marksta@umich.edu>
2024-03-23 20:19:44 -0400

    revert to one triangle

--- src/learnOpenGL.cpp
+++ src/learnOpenGL.cpp
@@ -49,14 +49,12 @@ int main(void)
     // ------------------------------------------------------------------
     float vertices[] = {
         // positions        // colors
-         0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // top right
+         0.0f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // top
          0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom right
         -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,  // bottom left
-        -0.5f,  0.5f, 0.0f , 1.0f, 1.0f, 1.0f   // top left
     };
     unsigned int indices[] = {  // note that we start from 0!
-        0, 1, 3,   // first triangle
-        1, 2, 3    // second triangle
+        0, 1, 2,   // first triangle
     };
 
     unsigned int VBO, VAO, EBO;

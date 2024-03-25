f80628be4ce212b5a7ebca106dad80527fea538e
Thomas Marks <marksta@umich.edu>
2024-03-25 10:32:36 -0400

    try out more complicated rules

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@ void processInput(Window window){
 int main(void){
 
     unsigned int width = 1000, height = 1000;
-    unsigned int pixelWidth = 100, pixelHeight = 100;
+    unsigned int pixelWidth = 256, pixelHeight = 256;
 
     // Create a window and check that it worked successfully
     Window window("OpenGL", width, height);
@@ -38,10 +38,15 @@ int main(void){
     // Create our ant and associated state
     LangtonAnt ant(
         pixelWidth, pixelHeight,
-        std::make_pair(0.5f, 0.5f),
+        std::make_pair(0.8f, 0.2f),
         0,
-        "LRRRRRLLR",
-        std::vector<Color>({BLACK, CYAN, MAGENTA, YELLOW, RED, GREEN, BLUE, WHITE, Color(0.5, 0.5, 0.5)})
+        "RRLLLRLLLRRR",
+        std::vector<Color>({
+            BLACK, CYAN, MAGENTA,
+            YELLOW, RED, GREEN,
+            BLUE, WHITE, Color(127, 127, 127),
+            Color(127, 0, 0), Color(0, 127, 0), Color(0, 0, 127)
+        })
     );
 
     // Render loop
